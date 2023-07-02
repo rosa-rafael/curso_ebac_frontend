40fb0858d07c243ad1578d2cc7d3200d99b6bf4a
Rafael Rosa <rafaelsahyoun@gmail.com>
2023-07-02 10:44:01 -0300

    Subindo o projeto para o repositorio git

---
+++ js/main.js
@@ -0,0 +1,64 @@
+$(document).ready(function () {
+    $('#carousel-imagens').slick({
+        autoplay: true,
+    });
+
+    $('.menu-hamburguer').click(function() {
+        $('nav').slideToggle();
+    })
+
+    $('#telefone').mask('(00) 00000-0000')
+    $('#cep').mask('00000-000')
+    $('#cpf').mask('000.000.000-00')
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            cpf: {
+                required: true
+            },
+            cep: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            mensagem: {
+                required: true
+            },
+            carroDeinterrese: {
+                required: false
+            },
+        },
+            messages: {
+                nome: 'Por favor, insira o seu nome.'
+            },
+            submitHandler: function(form) {
+                console.log(form)
+            },
+            invalidHandler: function(evento, validador) {
+                let camposIncorretos = validador.numberOfInvalids();
+                if(camposIncorretos) {
+                    alert(`Existem ${camposIncorretos} campos incorretos.`)
+                }
+            }
+        
+    })
+
+    $('.lista-veiculos button').click(function () {
+        const destino = $('#contato');
+        const nomeVeiculo = $(this).parent().find('h3').text();
+
+        $('#veiculo-interesse').val(nomeVeiculo);
+
+        $('html').animate({
+            scrollTop: destino.offset().top
+        }, 1000)
+      })
+})
